fa825da5cd46678bbf207065949decb0905015bb
Elian <eliancarlogm@gmail.com>
2022-05-09 13:08:00 -0430

    install needed package

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,64 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [count, setCount] = useState(0);
+   const url = "http://localhost:3002/api/admin/signup";
+   const data = {
+      firstName: "Eliangelica",
+      lastName: "Gonzalez Marcano",
+      email: "eliangelicagm@gmail.com",
+      password: "123456789",
+   };
+   fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+         "Content-Type": "application/json",
+      },
+   })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+   return (
+      <div className="App">
+         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+               <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+               <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="me-auto">
+                     <Nav.Link href="#features">Features</Nav.Link>
+                     <Nav.Link href="#pricing">Pricing</Nav.Link>
+                     <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">
+                           Action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">
+                           Another action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">
+                           Something
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">
+                           Separated link
+                        </NavDropdown.Item>
+                     </NavDropdown>
+                  </Nav>
+                  <Nav>
+                     <Nav.Link href="#deets">More deets</Nav.Link>
+                     <Nav.Link eventKey={2} href="#memes">
+                        Dank memes
+                     </Nav.Link>
+                  </Nav>
+               </Navbar.Collapse>
+            </Container>
+         </Navbar>
+      </div>
+   );
 }
 
-export default App
+export default App;
