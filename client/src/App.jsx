dc1ababadb7362c558343af929b3eebeb4fb6246
Elian <eliancarlogm@gmail.com>
2022-05-09 16:17:12 -0430

    Add bootstrap components on Home and Signin. Correction default styles of vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./containers/Home";
+import Signin from "./containers/Signin";
+import Signup from "./containers/Signup";
 
 function App() {
-   const [count, setCount] = useState(0);
-   const url = "http://localhost:3002/api/admin/signup";
+   /* const url = "http://localhost:3002/api/admin/signup";
    const data = {
       firstName: "Eliangelica",
       lastName: "Gonzalez Marcano",
@@ -20,43 +22,17 @@ function App() {
    })
       .then((res) => res.json())
       .then((data) => console.log(data))
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error)); */
 
    return (
       <div className="App">
-         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Container>
-               <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-               <Navbar.Collapse id="responsive-navbar-nav">
-                  <Nav className="me-auto">
-                     <Nav.Link href="#features">Features</Nav.Link>
-                     <Nav.Link href="#pricing">Pricing</Nav.Link>
-                     <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">
-                           Action
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">
-                           Another action
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">
-                           Something
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">
-                           Separated link
-                        </NavDropdown.Item>
-                     </NavDropdown>
-                  </Nav>
-                  <Nav>
-                     <Nav.Link href="#deets">More deets</Nav.Link>
-                     <Nav.Link eventKey={2} href="#memes">
-                        Dank memes
-                     </Nav.Link>
-                  </Nav>
-               </Navbar.Collapse>
-            </Container>
-         </Navbar>
+         <Router>
+            <Routes>
+               <Route path="/" exact element={<Home />} />
+               <Route path="/signin" element={<Signin />} />
+               <Route path="/signup" element={<Signup />} />
+            </Routes>
+         </Router>
       </div>
    );
 }
