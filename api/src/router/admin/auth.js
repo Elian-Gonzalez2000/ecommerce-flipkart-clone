cf5b7b216a0c278a674eeec31b239f4eb092fa43
Elian <eliancarlogm@gmail.com>
2022-05-10 00:38:50 -0430

    Fix route in route/admin/auth

--- api/src/router/admin/auth.js
+++ api/src/router/admin/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { signup, signin } = require("../../controller/admin/auth.js");
-const { validateSignupRequest, validateSigninRequest } = require("../../validators/auth.js");
+const {
+   validateSignupRequest,
+   validateSigninRequest,
+   isRequestValidated,
+} = require("../../validators/auth.js");
 
 router.post("/admin/signin", validateSigninRequest, signin);
 
