905ff95af1942201fedaa99f450c0c888992895d
Elian Gonzalez <eliancarlogm@gmail.com>
2023-09-26 23:15:02 -0400

    push mernstack ecommerce 42 changes

--- client/src/containers/CheckoutPage/index.jsx
+++ client/src/containers/CheckoutPage/index.jsx
@@ -171,6 +171,7 @@ const CheckoutPage = (props) => {
          totalAmount,
          items,
          paymentStatus: "pending",
+         paymentType: "cod",
       };
       console.log(payload);
       dispatch(addOrder(payload));
