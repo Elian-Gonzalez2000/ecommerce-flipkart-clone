9674b2f7e654e711343b233fa86ce42c8ecdbc29
unknown <eliancarlogm@gmail.com>
2024-08-31 00:28:14 +0200

    feat(./admin-app): Add error and confirm pages to sigup

---
+++ admin-app/src/containers/Signup/confirm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignupConfirm = () => {
+  return (
+    <section class="container">
+      <p class="alert_success">&#x2611; Cuenta confirmada correctamente</p>
+    </section>
+  );
+};
+
+export default SignupConfirm;
---
+++ admin-app/src/containers/Signup/error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignupError = () => {
+  return (
+    <section class="container">
+      <p class="alert_success">&#x2611; Cuenta confirmada correctamente</p>
+    </section>
+  );
+};
+
+export default SignupError;
