586a45f5a58741b808723d0d997175e7d0cd9711
unknown <eliancarlogm@gmail.com>
2024-09-03 00:09:01 +0200

    feat(./backend): Add get(/admin/confirm/:token) route

--- backend/src/router/admin/auth.js
+++ backend/src/router/admin/auth.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const { requiresSignin } = require("../../common-middleware/index.js");
 const router = express.Router();
-const { signup, signin, signout } = require("../../controller/admin/auth.js");
 const {
-   validateSignupRequest,
-   validateSigninRequest,
-   isRequestValidated,
+  signup,
+  signin,
+  signout,
+  confirm,
+} = require("../../controller/admin/auth.js");
+const {
+  validateSignupRequest,
+  validateSigninRequest,
+  isRequestValidated,
 } = require("../../validators/auth.js");
 
 router.post("/admin/signin", validateSigninRequest, signin);
@@ -13,5 +18,7 @@ router.post("/admin/signin", validateSigninRequest, signin);
 router.post("/admin/signup", validateSignupRequest, isRequestValidated, signup);
 
 router.post("/admin/signout", signout);
+
+router.get("/admin/confirm/:token", confirm);
 
 module.exports = router;
