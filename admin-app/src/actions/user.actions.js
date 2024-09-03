172f0f5da6622a2210f5b362b85b49ae16cd7079
unknown <eliancarlogm@gmail.com>
2024-09-03 21:23:18 +0200

    refactor(./admin-app): add try catch in signup action

--- admin-app/src/actions/user.actions.js
+++ admin-app/src/actions/user.actions.js
@@ -2,32 +2,36 @@ import { authConstants, userConstants } from "./constants";
 import axios from "../helpers/axios.js";
 
 export const signup = (user) => {
-   return async (dispatch) => {
+  return async (dispatch) => {
+    try {
       dispatch({ type: userConstants.USER_REGISTER_REQUEST });
       const res = await axios.post("/admin/signup", {
-         ...user,
+        ...user,
       });
       //console.log(res);
       if (res.status === 201) {
-         const { message } = res.data;
-         dispatch({
-            type: userConstants.USER_REGISTER_SUCCESS,
-            payload: { message },
-         });
-      } else {
-         if (res.status === 400) {
-            dispatch({
-               type: userConstants.USER_REGISTER_FAILURE,
-               payload: { error: res.data.message },
-            });
-         }
+        const { message } = res.data;
+        dispatch({
+          type: userConstants.USER_REGISTER_SUCCESS,
+          payload: { message },
+        });
       }
+    } catch (error) {
+      const { status, data } = error.response;
+      if (status === 400) {
+        console.log(error);
+        dispatch({
+          type: userConstants.USER_REGISTER_FAILURE,
+          payload: { error: data },
+        });
+      }
+    }
 
-      dispatch({
-         type: authConstants.LOGIN_REQUEST,
-         payload: {
-            ...user,
-         },
-      });
-   };
+    dispatch({
+      type: authConstants.LOGIN_REQUEST,
+      payload: {
+        ...user,
+      },
+    });
+  };
 };
