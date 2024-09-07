1c0a5f8d5cd7fb9ddd6dff3d986a42a1763df3ee
unknown <eliancarlogm@gmail.com>
2024-09-07 23:18:03 +0200

    feat(./admin-app): Add EditProduct actions in reducer

--- admin-app/src/reducers/product.reducer.js
+++ admin-app/src/reducers/product.reducer.js
@@ -1,17 +1,24 @@
 import { productConstants } from "../actions/constants";
 
 const initialState = {
-   products: [],
+  products: [],
 };
 
 export default (state = initialState, action) => {
-   switch (action.type) {
-      case productConstants.GET_ALL_PRODUCTS_SUCCESS:
-         state = {
-            ...state,
-            products: action.payload.products,
-         };
-         break;
-   }
-   return state;
+  switch (action.type) {
+    case productConstants.GET_ALL_PRODUCTS_SUCCESS:
+      state = {
+        ...state,
+        products: action.payload.products,
+      };
+      break;
+
+    case productConstants.EDIT_PRODUCT_SUCCESS:
+      state = {
+        ...state,
+        products: action.payload.products,
+      };
+      break;
+  }
+  return state;
 };
