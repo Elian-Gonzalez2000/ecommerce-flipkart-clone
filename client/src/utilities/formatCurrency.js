e267fc7d7022895d0d006929b2c1b554bf2f2c43
unknown <eliancarlogm@gmail.com>
2024-09-11 13:42:28 +0200

    feat(./client): improve utility accepting anything options

--- client/src/utilities/formatCurrency.js
+++ client/src/utilities/formatCurrency.js
@@ -1,16 +1,17 @@
-export default ({ currency = "", value, style = "currency" }) => {
-   if (style === "currency") {
-      const formatter = new Intl.NumberFormat("en-US", {
-         style,
-         maximumFractionDigits: 2,
-         currency,
-      });
-      return formatter.format(value);
-   }
-
-   const formatter = new Intl.NumberFormat("en-US", {
+export default ({ currency = "", value, style = "currency" }, options = {}) => {
+  if (style === "currency") {
+    const formatter = new Intl.NumberFormat("en-US", {
       style,
-      maximumFractionDigits: 2,
-   });
-   return formatter.format(value);
+      //  maximumFractionDigits: 2,
+      currency,
+      ...options,
+    });
+    return formatter.format(value);
+  }
+
+  const formatter = new Intl.NumberFormat("en-US", {
+    style,
+    maximumFractionDigits: 2,
+  });
+  return formatter.format(value);
 };
