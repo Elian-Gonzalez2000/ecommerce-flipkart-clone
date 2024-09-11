a1aedd2b190896558b4abbd652f405f2cd9be1bf
unknown <eliancarlogm@gmail.com>
2024-09-11 13:40:24 +0200

    feat(./client): call formatCurrency to improve price look

--- client/src/components/UI/Price.jsx
+++ client/src/components/UI/Price.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { BiRupee } from "react-icons/bi";
+import formatCurrency from "../../utilities/formatCurrency";
 
 const Price = (props) => {
-   const styles = {
-      fontSize: props.fontSize ? props.fontSize : "14px",
-      fontWeight: "bold",
-      margin: "5px 0",
-   };
-   return (
-      <span style={styles}>
-         <BiRupee />
-         {props.value}
-      </span>
-   );
+  const styles = {
+    fontSize: props.fontSize ? props.fontSize : "14px",
+    fontWeight: "bold",
+    margin: "5px 0",
+  };
+  return (
+    <span style={styles}>
+      {/* <BiRupee /> */}
+      {formatCurrency(
+        { currency: "INR", value: props.value },
+        { trailingZeroDisplay: "stripIfInteger" }
+      )}
+    </span>
+  );
 };
 
 export default Price;
