8ae1d0822d46db8e7d6f39e17ecd39fe2438a735
unknown <eliancarlogm@gmail.com>
2024-09-12 14:50:38 +0200

    perf(./client): Add breedIcon property into Breed component, corrrect total amount product and impro

--- client/src/containers/OrderPage/index.jsx
+++ client/src/containers/OrderPage/index.jsx
@@ -8,72 +8,64 @@ import Layout from "../../components/Layout";
 import Card from "../../components/UI/Card";
 import { Breed } from "../../components/MaterialUI";
 import "./style.css";
+import Price from "../../components/UI/Price";
+import { IoIosArrowForward } from "react-icons/io";
 
 const OrderPage = (props) => {
-   const user = useSelector((state) => state.user);
-   const dispatch = useDispatch();
-   useEffect(() => {
-      dispatch(getOrders());
-   }, []);
-   return (
-      <Layout>
-         <div>
-            <Breed
-               breed={[
-                  { name: "Home", href: "/" },
-                  { name: "My Account", href: "/account" },
-                  { name: "My Orders", href: "/account/orders" },
-               ]}
-            />
-            {user.orders.length > 0 &&
-               user.orders.map((order) => {
-                  return order.items.map((item) => (
-                     <Card
-                        key={randomUI()}
-                        style={{ display: "block", margin: "5px 0" }}
-                     >
-                        <Link
-                           to={`/order-details/${order._id}`}
-                           className="orderItemContainer"
-                        >
-                           <div
-                              style={{
-                                 display: "flex",
-                                 flex: "1",
-                                 justifyContent: "space-between",
-                              }}
-                           >
-                              <picture
-                                 style={{
-                                    width: "80px",
-                                    height: "80px",
-                                    overflow: "hidden",
-                                    textAlign: "center",
-                                 }}
-                              >
-                                 <img
-                                    style={{
-                                       maxWidth: "80px",
-                                       maxHeight: "80px",
-                                    }}
-                                    src={genericPublicUrl(
-                                       item.productId.productPictures[0].img
-                                    )}
-                                 />
-                              </picture>
-                              <p style={{ width: "300px" }}>
-                                 {item.productId.name}
-                              </p>
-                              <p>{item.payablePrice}</p>
-                              <p>{order.paymentStatus}</p>
-                           </div>
-                        </Link>
-                     </Card>
-                  ));
-               })}
-         </div>
-      </Layout>
-   );
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getOrders());
+  }, []);
+  return (
+    <Layout>
+      <div>
+        <Breed
+          breed={[
+            { name: "Home", href: "/" },
+            { name: "My Account", href: "/account" },
+            { name: "My Orders", href: "/account/orders" },
+          ]}
+          breedIcon={<IoIosArrowForward />}
+        />
+        {user.orders.length > 0 &&
+          user.orders.map((order) => {
+            return order.items.map((item) => (
+              <Card
+                key={randomUI()}
+                style={{ display: "block", margin: "5px 0" }}
+              >
+                <Link
+                  to={`/order-details/${order._id}`}
+                  className="order-item-container"
+                >
+                  {/* {console.log(item)} */}
+                  <div className="order-item-details">
+                    <picture>
+                      <img
+                        src={item.productId.productPictures[0].imgUrl}
+                        alt={item.productId.name}
+                      />
+                    </picture>
+                    <p className="order-item-name">{item.productId.name}</p>
+                    <p>
+                      {<Price value={item.payablePrice * item.purchasedQty} />}
+                    </p>
+                    <p
+                      className={`order-item-status ${
+                        order?.paymentStatus && order.paymentStatus
+                      }`}
+                    >
+                      {order.paymentStatus}
+                    </p>
+                  </div>
+                </Link>
+              </Card>
+            ));
+          })}
+      </div>
+    </Layout>
+  );
 };
 
 export default OrderPage;
