4c2100252409eade65a96c06ab8ffac07d88eb8b
unknown <eliancarlogm@gmail.com>
2024-09-12 17:27:47 +0200

    refactor(./client): delete imports not used, clean code

--- client/src/actions/auth.action.js
+++ client/src/actions/auth.action.js
@@ -3,98 +3,101 @@ import axios from "../helpers/axios.js";
 
 // new update signup action
 export const signup = (user) => {
-   return async (dispatch) => {
-      try {
-         dispatch({ type: authConstants.SIGNUP_REQUEST });
-         const res = await axios.post(`/signup`, user);
-         if (res.status === 201) {
-            dispatch({ type: authConstants.SIGNUP_SUCCESS });
-            const { token, user } = res.data;
-            localStorage.setItem("token", token);
-            localStorage.setItem("user", JSON.stringify(user));
-            dispatch({
-               type: authConstants.LOGIN_SUCCESS,
-               payload: {
-                  token,
-                  user,
-               },
-            });
-         } else {
-            const { error } = res.data;
-            dispatch({
-               type: authConstants.SIGNUP_FAILURE,
-               payload: { error },
-            });
-         }
-      } catch (error) {
-         const { data } = error.response;
-         dispatch({
-            type: authConstants.SIGNUP_FAILURE,
-            payload: { error: data.error },
-         });
+  return async (dispatch) => {
+    try {
+      dispatch({ type: authConstants.SIGNUP_REQUEST });
+      const res = await axios.post(`/signup`, user);
+      if (res.status === 201) {
+        dispatch({ type: authConstants.SIGNUP_SUCCESS });
+        const { token, user } = res.data;
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(user));
+        dispatch({
+          type: authConstants.LOGIN_SUCCESS,
+          payload: {
+            token,
+            user,
+          },
+        });
+      } else {
+        const { error } = res.data;
+        dispatch({
+          type: authConstants.SIGNUP_FAILURE,
+          payload: { error },
+        });
       }
-   };
+    } catch (error) {
+      console.log(error);
+      if (error?.response?.status == 400) {
+        const { data } = error.response;
+        dispatch({
+          type: authConstants.SIGNUP_FAILURE,
+          payload: { error: data.error },
+        });
+      }
+    }
+  };
 };
 
 export const login = (user) => {
-   return async (dispatch) => {
-      try {
-         dispatch({ type: authConstants.LOGIN_REQUEST });
-         const res = await axios.post("/signin", {
-            ...user,
-         });
-         //console.log(res);
-         if (res.status === 200) {
-            /* Take the token and user from the response, then save in localStorage with setItem, this permite use the token easly in the application */
-            const { token, user } = res.data;
-            localStorage.setItem("token", token);
-            localStorage.setItem("user", JSON.stringify(user));
-            dispatch({
-               type: authConstants.LOGIN_SUCCESS,
-               payload: { token, user },
-            });
-         }
-      } catch (error) {
-         if (error.response.status === 400) {
-            dispatch({
-               type: authConstants.LOGIN_FAILURE,
-               payload: { error: error.response.data.message },
-            });
-         }
+  return async (dispatch) => {
+    try {
+      dispatch({ type: authConstants.LOGIN_REQUEST });
+      const res = await axios.post("/signin", {
+        ...user,
+      });
+      //console.log(res);
+      if (res.status === 200) {
+        /* Take the token and user from the response, then save in localStorage with setItem, this permite use the token easly in the application */
+        const { token, user } = res.data;
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(user));
+        dispatch({
+          type: authConstants.LOGIN_SUCCESS,
+          payload: { token, user },
+        });
       }
-   };
+    } catch (error) {
+      if (error.response.status === 400) {
+        dispatch({
+          type: authConstants.LOGIN_FAILURE,
+          payload: { error: error.response.data.message },
+        });
+      }
+    }
+  };
 };
 
 export const isUserLoggedIn = () => {
-   return async (dispatch) => {
-      const token = localStorage.getItem("token");
+  return async (dispatch) => {
+    const token = localStorage.getItem("token");
 
-      if (token) {
-         const user = JSON.parse(localStorage.getItem("user"));
-         dispatch({
-            type: authConstants.LOGIN_SUCCESS,
-            payload: { token, user },
-         });
-      } else {
-         dispatch({
-            type: authConstants.LOGIN_FAILURE,
-            payload: { error: "Fail to login" },
-         });
-      }
-   };
+    if (token) {
+      const user = JSON.parse(localStorage.getItem("user"));
+      dispatch({
+        type: authConstants.LOGIN_SUCCESS,
+        payload: { token, user },
+      });
+    } else {
+      dispatch({
+        type: authConstants.LOGIN_FAILURE,
+        payload: { error: "Fail to login" },
+      });
+    }
+  };
 };
 
 export const signout = () => {
-   return async (dispatch) => {
-      dispatch({ type: authConstants.LOGOUT_REQUEST });
-      //const res = await axios.post("/signout");
-      // localStorage.removeItem("user");
-      // localStorage.removeItem("token");
-      localStorage.clear();
-      dispatch({ type: authConstants.LOGOUT_SUCCESS });
-      dispatch({ type: cartConstants.RESET_CART });
+  return async (dispatch) => {
+    dispatch({ type: authConstants.LOGOUT_REQUEST });
+    //const res = await axios.post("/signout");
+    // localStorage.removeItem("user");
+    // localStorage.removeItem("token");
+    localStorage.clear();
+    dispatch({ type: authConstants.LOGOUT_SUCCESS });
+    dispatch({ type: cartConstants.RESET_CART });
 
-      /* if (res.status === 200) {
+    /* if (res.status === 200) {
          localStorage.clear();
          dispatch({
             type: authConstants.LOGOUT_SUCCESS,
@@ -105,5 +108,5 @@ export const signout = () => {
             payload: { error: res.data.error },
          });
       } */
-   };
+  };
 };
--- client/src/actions/category.action.js
+++ client/src/actions/category.action.js
@@ -2,28 +2,28 @@ import axios from "../helpers/axios.js";
 import { categoryConstants } from "./constants.js";
 
 export const getAllCategory = () => {
-   return async (dispatch) => {
-      try {
-         dispatch({ type: categoryConstants.GET_ALL_CATEGORIES_REQUEST });
-         const res = await axios.get("category/getcategory");
-         console.log(res);
+  return async (dispatch) => {
+    try {
+      dispatch({ type: categoryConstants.GET_ALL_CATEGORIES_REQUEST });
+      const res = await axios.get("category/getcategory");
+      //  console.log(res);
 
-         if (res.status === 200) {
-            const { categoryList } = res.data;
-            dispatch({
-               type: categoryConstants.GET_ALL_CATEGORIES_SUCCESS,
-               payload: { categories: categoryList },
-            });
-         }
-      } catch (err) {
-         const { status, data } = err.response;
-         console.log(data);
-         if (status === 400) {
-            dispatch({
-               type: authConstants.LOGIN_FAILURE,
-               payload: { err: data.message },
-            });
-         }
+      if (res.status === 200) {
+        const { categoryList } = res.data;
+        dispatch({
+          type: categoryConstants.GET_ALL_CATEGORIES_SUCCESS,
+          payload: { categories: categoryList },
+        });
       }
-   };
+    } catch (err) {
+      const { status, data } = err.response;
+      console.log(data);
+      if (status === 400) {
+        dispatch({
+          type: authConstants.LOGIN_FAILURE,
+          payload: { err: data.message },
+        });
+      }
+    }
+  };
 };
