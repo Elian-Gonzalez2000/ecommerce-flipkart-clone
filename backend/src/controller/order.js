060ecfedf2944eef6008f23eb3a6cc3769008f12
unknown <eliancarlogm@gmail.com>
2024-10-09 19:15:54 +0200

    feat(./backend): create deleteOrder controller and user id in addOrder can be taked from req.user._i

--- backend/src/controller/order.js
+++ backend/src/controller/order.js
@@ -8,7 +8,7 @@ exports.addOrder = (req, res) => {
   Cart.deleteOne({ user: req.user_id }).exec((error, result) => {
     if (error) return res.status(400).json({ error });
     if (result) {
-      req.body.user = req.user._id;
+      req.body.user = req.user._id || req.body.user;
       req.body.orderStatus = [
         {
           type: "ordered",
@@ -30,7 +30,7 @@ exports.addOrder = (req, res) => {
       ];
       const order = new Order(req.body);
       order.save((error, order) => {
-        if (error) return res.status(400).json({ error });
+        if (error) return res.status(400).json({ error: error });
         if (order) {
           res.status(201).json({ order });
         }
@@ -39,55 +39,14 @@ exports.addOrder = (req, res) => {
   });
 };
 
-// const stripe = Stripe(process.env.SECRET_KEY_STRIPE);
-
-// exports.addStripeOrder = async (req, res) => {
-//   const { items } = req.body;
-
-//   try {
-//     const session = await stripe.checkout.sessions.create({
-//       payment_method_types: ["card"],
-//       line_items: items.map((item) => ({
-//         price_data: {
-//           currency: "eur",
-//           product_data: {
-//             name: item.productId,
-//           },
-//           unit_amount: item.payablePrice,
-//         },
-//         quantity: item.purchasedQty,
-//       })),
-//       mode: "payment",
-//       success_url: "http://localhost:3000/",
-//       cancel_url: "http://localhost:3000/cart",
-//     });
-
-//     res.status(200).json({ id: session.id });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// exports.getStripeCheckoutSessionWebhook = (req, res) => {
-//   const signature = req.headers["stripe-signature"];
-//   let event;
-
-//   try {
-//     event = stripe.webhooks.constructEvent(
-//       req.body,
-//       signature,
-//       "whsec_bce21eabafa80fe15348cd171fce9aa2bdd0eb409639a05db966fb09834e9539"
-//     );
-//     console.log("event: ", event);
-
-//     if (event.object.object == "checkout.session") {
-//       return res.status(200).json({ event });
-//     }
-//   } catch (err) {
-//     console.error("Webhook signature verification failed.", err);
-//     return res.status(400).send(`Webhook Error: ${err.message}`);
-//   }
-// };
+exports.deleteOrder = (req, res) => {
+  Order.deleteOne({ _id: req.body.orderId }).exec((error, result) => {
+    if (error) return res.status(400).json({ error });
+    if (result) {
+      res.status(202).json({ message: "Order deleted", result });
+    }
+  });
+};
 
 exports.getOrders = (req, res) => {
   Order.find({ user: req.user._id })
