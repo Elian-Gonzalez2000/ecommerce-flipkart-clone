2511a32d1db1fa279e48b7d6d6743193ef24848c
unknown <eliancarlogm@gmail.com>
2024-10-09 19:18:42 +0200

    feat(./backend): addStripeOrder work, send a request to add order to get the expecific payment did i

--- backend/src/controller/stripe.js
+++ backend/src/controller/stripe.js
@@ -1,11 +1,23 @@
 const Stripe = require("stripe");
+const axios = require("axios");
 
 const stripe = Stripe(process.env.SECRET_KEY_STRIPE);
 
 exports.addStripeOrder = async (req, res) => {
   const { items } = req.body;
+  console.log("authorization", req.headers.authorization);
 
   try {
+    const order = await axios({
+      method: "POST",
+      baseURL: "http://localhost:3002/api",
+      url: "/add-order",
+      data: req.body,
+      headers: {
+        Authorization: req.headers.authorization,
+      },
+    });
+
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items: items.map((item) => ({
@@ -19,13 +31,15 @@ exports.addStripeOrder = async (req, res) => {
         quantity: item.purchasedQty,
       })),
       mode: "payment",
-      success_url: "http://localhost:3000/",
-      cancel_url: "http://localhost:3000/cart",
+      success_url: "http://localhost:2000/checkout/success",
+      cancel_url: `http://localhost:2000/checkout/cancel/${order.data.order._id}`,
     });
 
-    res.status(200).json({ id: session.id });
+    if (session && order.status == 201) {
+      return res.status(200).json({ id: session.id });
+    }
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(400).json({ message: "something went wrong", error: error });
   }
 };
 
@@ -39,7 +53,7 @@ exports.getStripeCheckoutSessionWebhook = (req, res) => {
       signature,
       "whsec_bce21eabafa80fe15348cd171fce9aa2bdd0eb409639a05db966fb09834e9539"
     );
-    console.log("event: ", event);
+    // console.log("event: ", event);
 
     if (event) {
       return res.status(200).json({ event });
