bc75a41bcf4f9386c5e5b5061389c923b945f476
unknown <eliancarlogm@gmail.com>
2024-10-12 00:13:55 +0200

    feat(./client): Clean large comment and add cancel and success chekout routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,50 +10,11 @@ import CartPage from "./containers/CartPage";
 import CheckoutPage from "./containers/CheckoutPage";
 import OrderPage from "./containers/OrderPage";
 import OrderDetailsPage from "./containers/OrderDetailsPage";
-import SuccessCheckout from "./containers/CheckoutPage/Success";
-import CancelCheckout from "./containers/CheckoutPage/Cancel";
+import SuccessCheckout from "./containers/CheckoutPage/CheckoutResults/Success";
+import CancelCheckout from "./containers/CheckoutPage/CheckoutResults/Cancel";
 
 function App() {
   const url = "http://localhost:3002/api/admin/signin";
-  // const data = {
-  //    /*       firstName: "Edwiin",
-  //    lastName: "Gonzalez", */
-  //    /* email: "eliancarlogm@gmail.com",
-  //    password: "123456789", */
-  //    //name: "Electronics",
-  //    //parentId: "627b1a44874b7eba250c33d8",
-  // };
-  // fetch(url, {
-  //    method: "POST",
-  //    body: JSON.stringify({
-  //       email: "eswin@gmail.com",
-  //       password: "123456789",
-  //    }),
-  //    headers: {
-  //       "Content-Type": "application/json",
-  //    },
-  // })
-  //    .then((res) => res.json())
-  //    .then((data) => {
-  //       console.log(data);
-  //       if (data?.token) {
-  //          console.log(data.token);
-  //          fetch("http://localhost:3002/api/category/create", {
-  //             method: "POST",
-  //             body: JSON.stringify({ name: "Sports" }),
-  //             headers: {
-  //                "Content-Type": "application/json",
-  //                authorization:
-  //                   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjdiMWE0NDg3NGI3ZWJhMjUwYzMzZDgiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NTIyMzUxNzQsImV4cCI6MTY1MjI0MjM3NH0.EfNoWqYCmu210ryhnQcXSAau3e3lUwLMDBqiq9ZUp9o",
-  //             },
-  //          })
-  //             .then((res) => res.json())
-  //             .then((data) => console.log(data))
-  //             .catch((err) => console.log(err));
-  //       }
-  //    })
-  //    .catch((error) => console.log(error));
-
   const dispatch = useDispatch();
   const auth = useSelector((state) => state.auth);
 
@@ -90,7 +51,11 @@ function App() {
             element={<ProductsDetailsPage />}
           />
           <Route path="/:slug" exact element={<ProductListPage />} />
-          <Route path="/checkout/success" exact element={<SuccessCheckout />} />
+          <Route
+            path="/checkout/success/:orderId"
+            exact
+            element={<SuccessCheckout />}
+          />
           <Route
             path="/checkout/cancel/:orderId"
             exact
