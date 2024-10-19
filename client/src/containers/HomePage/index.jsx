ba7ddd6d1d9312b14738064c711e727e092682a7
unknown <eliancarlogm@gmail.com>
2024-10-20 00:42:39 +0200

    feat(./client): Add carousel components with react-boostrap and add it in homepage with his styles

--- client/src/containers/HomePage/index.jsx
+++ client/src/containers/HomePage/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import Carrousel from "../../components/UI/Carrousel/Carrousel";
 
 const HomePage = (props) => {
-   return (
-      <Layout>
-         <h1>Home page</h1>
-      </Layout>
-   );
+  return (
+    <Layout>
+      <Carrousel />
+    </Layout>
+  );
 };
 
 export default HomePage;
