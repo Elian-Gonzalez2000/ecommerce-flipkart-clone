b5c931f78c5b0a5ade2ffe09eacb7631eb5c2a41
unknown <eliancarlogm@gmail.com>
2024-10-23 23:50:37 +0200

    fix(./backend): Add another template for users

--- backend/src/config/mail.config.js
+++ backend/src/config/mail.config.js
@@ -51,7 +51,26 @@ const getTemplate = (name, token) => {
       `;
 };
 
+const getTemplateUser = (name, token) => {
+  return `
+        <head>
+            <link rel="stylesheet" href="./style.css">
+        </head>
+        
+        <div id="email___content">
+            <img src="https://i.imgur.com/eboNR82.png" alt="">
+            <h2>Hola ${name}</h2>
+            <p>Para confirmar tu cuenta, ingresa al siguiente enlace</p>
+            <a
+                href="http://localhost:3002/api/user/confirm/${token}"
+                target="_blank"
+            >Confirmar Cuenta</a>
+        </div>
+      `;
+};
+
 module.exports = {
   sendEmail,
   getTemplate,
+  getTemplateUser,
 };
