6ee7e301fdf8b2c61e1c4868123b07c39580ee4b
unknown <eliancarlogm@gmail.com>
2024-10-23 23:44:39 +0200

    feat(./client): Create error and confirm pages for the signup

---
+++ client/src/containers/Signup/Confirm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Success = () => {
+  return <>Hola</>;
+};
+
+export default Success;
---
+++ client/src/containers/Signup/Error.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Error = () => {
+  return <>Hola</>;
+};
+
+export default Error;
