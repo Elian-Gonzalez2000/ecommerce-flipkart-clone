985bf33d369f8e6e9a66ad1d00544cb41bae321e
unknown <eliancarlogm@gmail.com>
2024-10-23 18:34:47 +0200

    feat(./client): import PremiumProduct Component into home page

--- client/src/containers/HomePage/index.jsx
+++ client/src/containers/HomePage/index.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../../components/Layout";
 import Carrousel from "../../components/UI/Carrousel/Carrousel";
+import PremiumProducts from "../../components/PremiumProducts";
 
 const HomePage = (props) => {
   return (
     <Layout>
       <Carrousel />
+      <PremiumProducts />
     </Layout>
   );
 };
