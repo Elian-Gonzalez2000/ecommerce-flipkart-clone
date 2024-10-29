d3651eb4fb131b27815e7abdf9c1044311b89b15
unknown <eliancarlogm@gmail.com>
2024-10-29 22:41:35 +0100

    feat(./client): Add component to render return btn

---
+++ client/src/components/UI/ReturnButton/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BsFillArrowLeftSquareFill } from "react-icons/bs";
+import "./style.css";
+
+function ReturnButton() {
+  return (
+    <>
+      <BsFillArrowLeftSquareFill />
+    </>
+  );
+}
+
+export default ReturnButton;
