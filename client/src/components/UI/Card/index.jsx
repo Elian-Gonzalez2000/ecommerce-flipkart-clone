9505a1b59af2397256247ff5b5ef5796f060fe9f
unknown <eliancarlogm@gmail.com>
2024-11-10 23:49:30 +0100

    fix(./client): add prop header to show it or not

--- client/src/components/UI/Card/index.jsx
+++ client/src/components/UI/Card/index.jsx
@@ -2,31 +2,32 @@ import React from "react";
 import "./style.css";
 
 function Card({
-   headerLeft,
-   headerRight,
-   cardIcon,
-   priceRange,
-   classNames,
-   ...props
+  header = true,
+  headerLeft,
+  headerRight,
+  cardIcon,
+  priceRange,
+  classNames,
+  ...props
 }) {
-   return (
-      <article className={`card ${classNames ? classNames : ""}`} {...props}>
-         {
-            <div className="card-header">
-               {headerLeft && (
-                  <div className="header-left">
-                     {headerLeft}
-                     {cardIcon && cardIcon}
-                     {priceRange && priceRange}
-                  </div>
-               )}
-               {headerRight && headerRight}
+  return (
+    <article className={`card ${classNames ? classNames : ""}`} {...props}>
+      {header && (
+        <div className="card-header">
+          {headerLeft && (
+            <div className="header-left">
+              {headerLeft}
+              {cardIcon && cardIcon}
+              {priceRange && priceRange}
             </div>
-         }
+          )}
+          {headerRight && headerRight}
+        </div>
+      )}
 
-         {props.children}
-      </article>
-   );
+      {props.children}
+    </article>
+  );
 }
 
 export default Card;
