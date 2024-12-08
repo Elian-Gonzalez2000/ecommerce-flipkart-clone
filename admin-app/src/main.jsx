179c9971b1415768170fe62c6e0f68b961eecb04
unknown <eliancarlogm@gmail.com>
2024-12-08 19:06:36 +0100

    Update router to HashRouter

--- admin-app/src/main.jsx
+++ admin-app/src/main.jsx
@@ -4,16 +4,16 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 window.store = store;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-   <Provider store={store}>
-      <Router>
-         <React.StrictMode>
-            <App />
-         </React.StrictMode>
-      </Router>
-   </Provider>
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>
 );
