98e7d0f6b06e53d65f7064f268979a7e39aceed3
unknown <eliancarlogm@gmail.com>
2024-12-08 13:10:24 +0100

    feat(./client): Add metatags in the aplication

--- client/src/containers/CheckoutPage/index.jsx
+++ client/src/containers/CheckoutPage/index.jsx
@@ -23,6 +23,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements, useStripe, CardElement } from "@stripe/react-stripe-js";
 import { PUBLIC_KEY_STRIPE } from "../../urlConfig";
 import axios from "../../helpers/axios";
+import { Helmet } from "react-helmet";
 const stripePromise = loadStripe(PUBLIC_KEY_STRIPE);
 
 const CheckoutStep = (props) => {
@@ -316,6 +317,45 @@ const CheckoutPage = () => {
   return (
     <Elements stripe={stripePromise}>
       <Layout>
+        <Helmet>
+          <title>
+            {
+              "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!"
+            }
+          </title>
+          <meta property="og:title" content="Flipkart Clone" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:url"
+            content="https://elian-gonzalez2000.github.io/ecommerce-flipkart-clone/client/dist/"
+          />
+          <meta
+            property="og:image"
+            content="https://firebasestorage.googleapis.com/v0/b/ecommerce-flipkart-clone.appspot.com/o/general%2Fflipkart-metatags.png?alt=media&token=ae918f5a-699d-4b31-8963-ba9b6e2a4167"
+          />
+          <meta
+            property="og:description"
+            content="Online Shopping India Mobile, Cameras, Lifestyle &amp; more Online @ Flipkart.com"
+          />
+
+          <meta
+            name="twitter:url"
+            content="https://elian-gonzalez2000.github.io/ecommerce-flipkart-clone/client/dist/"
+          />
+          <meta name="twitter:title" content="Flipkart Clone" />
+          <meta
+            name="twitter:description"
+            content="Online Shopping India Mobile, Cameras, Lifestyle &amp; more Online @ Flipkart.com"
+          />
+          <meta
+            name="twitter:image"
+            content="https://firebasestorage.googleapis.com/v0/b/ecommerce-flipkart-clone.appspot.com/o/general%2Fflipkart-metatags.png?alt=media&token=ae918f5a-699d-4b31-8963-ba9b6e2a4167"
+          />
+          <link
+            rel="canonical"
+            href="https://elian-gonzalez2000.github.io/ecommerce-flipkart-clone/client/dist/"
+          />
+        </Helmet>
         {user.loading && <Loader />}
         {
           <div
