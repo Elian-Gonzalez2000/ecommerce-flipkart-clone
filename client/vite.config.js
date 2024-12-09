b5a6a2f982985322020f485a81354a4c8e91ef41
unknown <eliancarlogm@gmail.com>
2024-12-09 19:32:00 +0100

    fix(Root): Vite.config update, base url in build ./

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   server: {
-      port: 2000,
-   },
-   plugins: [react()],
+  server: {
+    port: 2000,
+  },
+  plugins: [react()],
+  base: "./",
 });
