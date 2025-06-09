fb9936302754b90e387dcebc6bc9361fbb78a4c7
unknown <eliancarlogm@gmail.com>
2025-06-09 22:48:21 +0200

    feat(client/src/components/MenuHeader/index.jsx): optimize mega menu structure, spacing, and responsive breakpoints for better layout

--- client/src/components/MenuHeader/index.jsx
+++ client/src/components/MenuHeader/index.jsx
@@ -1,46 +1,76 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./styles.css";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllCategory } from "../../actions";
 import { Link } from "react-router-dom";
 import { BsChevronDown } from "react-icons/bs";
 
-const MenuHeader = (props) => {
+const MenuHeader = () => {
   const category = useSelector((state) => state.category);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAllCategory());
-  }, []);
+  }, [dispatch]);
 
-  const renderCategories = (categoryList) => {
-    let myCategories = [];
-    for (let category of categoryList) {
-      myCategories.push(
-        <li key={category.name}>
-          {category.parentId ? (
+  // Renderiza el mega menú de subcategorías
+  const renderMegaMenu = (children) => (
+    <div className="mega-menu">
+      {children.map((subcat) => (
+        <>
+          <div
+            className="mega-menu-item mega-menu-subcategory"
+            key={subcat._id}
+          >
             <Link
-              to={`/${category.slug}?cid=${category._id}&type=${category.type}`}
+              className="mega-menu-title"
+              to={`/${subcat.slug}?cid=${subcat._id}&type=${subcat.type}`}
             >
-              {category.name}
+              {subcat.name}
             </Link>
-          ) : (
-            <span>
-              {category.name} {<BsChevronDown />}
-            </span>
+          </div>
+          {subcat.children && subcat.children.length > 0 && (
+            <>
+              {subcat.children.map((child) => (
+                <div className="mega-menu-item" key={child._id}>
+                  <Link
+                    to={`/${child.slug}?cid=${child._id}&type=${child.type}`}
+                  >
+                    {child.name}
+                  </Link>
+                </div>
+              ))}
+            </>
           )}
-          {category.children.length > 0 ? (
-            <ul>{renderCategories(category.children)}</ul>
-          ) : null}
-        </li>
-      );
-    }
-    return myCategories;
-  };
+        </>
+      ))}
+    </div>
+  );
+
+  // Renderiza las categorías principales
+  const renderCategories = (categoryList) =>
+    categoryList.map((cat) => (
+      <li key={cat._id} className="menu-item">
+        <span className="menu-link">
+          <Link
+            to={`/${cat.slug}?cid=${cat._id}&type=${cat.type}`}
+            className="main-category-link"
+          >
+            {cat.name}
+          </Link>
+          {cat.children && cat.children.length > 0 && (
+            <BsChevronDown className="chevron-icon" />
+          )}
+        </span>
+        {cat.children &&
+          cat.children.length > 0 &&
+          renderMegaMenu(cat.children)}
+      </li>
+    ));
 
   return (
     <div className="menu-header">
-      <ul>
+      <ul className="menu-list">
         {category.categories.length > 0
           ? renderCategories(category.categories)
           : null}
