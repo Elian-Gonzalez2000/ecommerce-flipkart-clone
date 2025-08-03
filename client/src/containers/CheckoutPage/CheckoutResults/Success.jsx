781285b00a12bcbb3dfd914526225f711eab12b1
unknown <eliancarlogm@gmail.com>
2025-08-03 19:29:27 +0200

    refactor(./client): Improve UI with skeleton for loader
    client/src/containers/CheckoutPage/CheckoutResults/Success.jsx

--- client/src/containers/CheckoutPage/CheckoutResults/Success.jsx
+++ client/src/containers/CheckoutPage/CheckoutResults/Success.jsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { getOrder, resetCart } from "../../../actions";
 import Price from "../../../components/UI/Price";
 import { Link } from "react-router-dom";
+import { isNotEmpty } from "../../../helpers/evalEmptyObjects";
+import Skeleton from "../../../components/UI/Skeleton/index.jsx";
 
 function SuccessCheckout() {
   const params = useParams();
@@ -18,21 +20,45 @@ function SuccessCheckout() {
   return (
     <Layout style={{ padding: "0", marginTop: "0" }}>
       <section className={"checkout-success-container"}>
-        <h2>Order Placed</h2>
-        <p style={{ fontSize: "1.1rem" }}>
-          Total price for {order.items && order.items.length} items{" "}
-          {<Price value={order.totalAmount} />}
-        </p>
-        <Link
-          style={{ color: "var(--text-light-color)", fontSize: "1.1rem" }}
-          to={`/order-details/${params.orderId}`}
-        >
-          View details{" "}
-        </Link>
+        {isNotEmpty(order) ? (
+          <h2>Order Placed</h2>
+        ) : (
+          <Skeleton
+            width="160px"
+            height="28px"
+            styles={{ margin: ".5rem 11px" }}
+          />
+        )}
+        {isNotEmpty(order) ? (
+          <p style={{ fontSize: "1.1rem" }}>
+            Total price for {order.items && order.items.length} items{" "}
+            {<Price value={order.totalAmount} />}
+          </p>
+        ) : (
+          <Skeleton
+            width="200px"
+            height="28px"
+            styles={{ margin: ".5rem 11px" }}
+          />
+        )}
+        {isNotEmpty(order) ? (
+          <Link
+            style={{ color: "var(--text-light-color)", fontSize: "1.1rem" }}
+            to={`/order-details/${params.orderId}`}
+          >
+            View details{" "}
+          </Link>
+        ) : (
+          <Skeleton
+            width="160px"
+            height="28px"
+            styles={{ margin: ".5rem 11px" }}
+          />
+        )}
       </section>
       <section className="products-section">
         <h2>Your orderned items</h2>
-        {order.items &&
+        {order.items ? (
           order.items.map((item) => {
             return (
               <article className="product-container" key={item._id}>
@@ -52,7 +78,36 @@ function SuccessCheckout() {
                 </div>
               </article>
             );
-          })}
+          })
+        ) : (
+          <>
+            <Skeleton
+              width="250px"
+              height="300px"
+              styles={{ margin: ".5rem 1rem" }}
+            />
+            <Skeleton
+              width="250px"
+              height="300px"
+              styles={{ margin: ".5rem 1rem" }}
+            />
+            <Skeleton
+              width="250px"
+              height="300px"
+              styles={{ margin: ".5rem 1rem" }}
+            />
+            <Skeleton
+              width="250px"
+              height="300px"
+              styles={{ margin: ".5rem 1rem" }}
+            />
+            <Skeleton
+              width="250px"
+              height="300px"
+              styles={{ margin: ".5rem 1rem" }}
+            />
+          </>
+        )}
       </section>
     </Layout>
   );
