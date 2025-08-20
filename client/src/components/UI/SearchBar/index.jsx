f33871f88c130fc44d924e03eaac251b5b9b2232
unknown <eliancarlogm@gmail.com>
2025-08-21 00:40:25 +0200

    feat(./client): Search bar now is functional.
    client/src/components/UI/SearchBar/index.jsx

--- client/src/components/UI/SearchBar/index.jsx
+++ client/src/components/UI/SearchBar/index.jsx
@@ -1,14 +1,37 @@
+import { useState, useRef } from "react";
 import "./styles.css";
 import { IoIosSearch } from "react-icons/io";
+import { Link, useNavigate } from "react-router-dom";
 
 function SearchBar() {
+  const [searchBar, setSearchBar] = useState("");
+  const searchBarInputRef = useRef(null);
+  const navigate = useNavigate();
+
+  const handleSearchBarInput = (e) => {
+    if (e.type == "change") {
+      //console.log(searchBarInputRef.current.value);
+    }
+    if (e.type == "click" || e.key === "Enter") {
+      const searchValue = searchBarInputRef.current.value;
+      if (searchValue.trim())
+        navigate(`/search?q=${searchValue}&sort=popularity`);
+      /* console.log("Click: ", searchBarInputRef.current.value); */
+    }
+  };
   return (
     <div className="search-input-container">
       <input
+        ref={searchBarInputRef}
         className="search-input"
         placeholder={"search for products, brands and more"}
+        onChange={(e) => handleSearchBarInput(e)}
+        onKeyUp={(e) => handleSearchBarInput(e)}
       />
-      <div className="search-icon-container">
+      <div
+        className="search-icon-container"
+        onClick={(e) => handleSearchBarInput(e)}
+      >
         <IoIosSearch
           style={{
             color: "#2874f0",
