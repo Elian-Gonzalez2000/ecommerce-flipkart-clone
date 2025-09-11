7d012a249ccd5ff5c8c7c3f1cb14049e93605bd7
unknown <eliancarlogm@gmail.com>
2025-09-11 22:44:45 +0200

    feat(./admin-app): Add create and getAll actions for homepagesCards
    admin-app/src/actions/homepageCards.js

---
+++ admin-app/src/actions/homepageCards.js
@@ -0,0 +1,51 @@
+import axios from "../helpers/axios.js";
+import { homepageCardsConstants } from "./constants.js";
+
+export const createHomepageCard = (formData) => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: homepageCardsConstants.CREATE_HOMEPAGECARD_REQUEST });
+      const res = await axios.post("admin/homepagecard/create", {
+        ...formData,
+        productsList: formData.products,
+      });
+
+      if (res.status === 201) {
+        dispatch({
+          type: homepageCardsConstants.CREATE_HOMEPAGECARD_SUCCESS,
+          payload: res.data.data,
+        });
+      }
+    } catch (error) {
+      if (error?.response?.status === 400) {
+        console.log(error);
+        dispatch({
+          type: homepageCardsConstants.CREATE_HOMEPAGECARD_FAILURE,
+          payload: error.response,
+        });
+      }
+    }
+  };
+};
+
+export const getAllCardsHomepages = () => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: homepageCardsConstants.GET_HOMEPAGECARDS_REQUEST });
+      const res = await axios.get("admin/homepagecard/getall");
+      if (res.status === 200) {
+        dispatch({
+          type: homepageCardsConstants.GET_HOMEPAGECARDS_SUCCESS,
+          payload: { allCards: res.data.allCards },
+        });
+      }
+    } catch (error) {
+      if (error?.response?.status === 400) {
+        dispatch({
+          type: homepageCardsConstants.GET_HOMEPAGECARDS_FAILURE,
+          payload: error.response,
+        });
+      }
+    }
+  };
+};
