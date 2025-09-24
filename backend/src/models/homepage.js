38cd5f232522d6711b30a779310b561727fa7520
unknown <eliancarlogm@gmail.com>
2025-09-24 23:49:29 +0200

    feat(./backend): Add property updatedBy
    backend/src/models/homepage.js

--- backend/src/models/homepage.js
+++ backend/src/models/homepage.js
@@ -25,6 +25,11 @@ const homepageSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
+
+    updatedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
   { timestamps: true }
 );
